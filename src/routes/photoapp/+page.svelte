58c912771b5cf6f6ac80d3abf1c78427374bcd4b
khaeduali <alikha06@edu.umea.se>
2025-11-06 10:47:22 +0100

    fin&build photoapp

--- src/routes/photoapp/+page.svelte
+++ src/routes/photoapp/+page.svelte
@@ -2,7 +2,10 @@
 let image = [
     "https://www.shutterstock.com/image-photo/these-some-random-photos-260nw-2402066699.jpg",
     "https://miro.medium.com/v2/resize:fit:1400/0*YeaUsQyhXSL1TCTH.png",
-    "https://i.redd.it/vd9dc7wfk9471.png"
+    "https://i.redd.it/vd9dc7wfk9471.png",
+    "https://cdn.pixabay.com/photo/2015/10/21/18/54/photos-at-random-1000224_1280.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEzSKHKnhS45Z7nEl040UNye83x9wQG08pw&s",
+    "https://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg"
 ]
 </script>
 
@@ -13,29 +16,33 @@ let image = [
             <img src={image[0]} alt="a">
         </div>
         <div class="imgwrapper">
-            <img src={image[0]} alt="a">
-        </div>
-        <div class="imgwrapper">
-            <img src={image[2]} alt="a">
-        </div>
-        <div class="imgwrapper">
             <img src={image[1]} alt="a">
         </div>
         <div class="imgwrapper">
             <img src={image[2]} alt="a">
+        </div>
+        <div class="imgwrapper">
+            <img src={image[3]} alt="a">
+        </div>
+        <div class="imgwrapper">
+            <img src={image[4]} alt="a">
+        </div>
+        <div class="imgwrapper">
+            <img src={image[5]} alt="a">
         </div>
     </div>
 </main>
 
 <style>
 .imgwrapper {
-    flex-grow: 1;
+    flex-grow: 0;
     margin: 10px;
     transition: all 0.2s;
+    padding: 30px;
 }
 
 .imgwrapper:hover {
-    flex-grow: 2;
+    transform: scale(1.1);
 }
 
 .imgwrapper:active {
@@ -45,6 +52,10 @@ let image = [
 .imgwrapper img {
     min-width: 300px;
     min-height: 300px;
+    
+    max-width: 600px;
+    max-height: 600px;
+
     width: 100%;
     border-radius: 20px;
     border: 2px dashed #b4befe;
@@ -57,6 +68,7 @@ let image = [
     display: flex;
     flex-wrap: wrap;
     align-items: center;
+    align-content: center;
     justify-content: center;
 }
 
